4f182e46858cab85c46add17cdc0bc0eae139d99
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-11-02 22:21:37 -0700

    removed par=t ofpath

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,19 +3,19 @@ const projects = [
         title: "movie-tickets-site",
         description: "MVC and MySQL",
         link: "https://github.com/Matt11Moriarty/movie-tickets-site",
-        gif: "./public/images/classic_flix.gif"
+        gif: "/public/images/classic_flix.gif"
     },
     {
         title: "social-network-api",
         description: "MongoDB and API routing",
         link: "https://github.com/Matt11Moriarty/social-network-api",
-        gif: "./public/images/social_network_api.gif"
+        gif: "/public/images/social_network_api.gif"
     },
     {
         title: "tech-blog",
         description: "Deployed full stack blog",
         link: "https://github.com/Matt11Moriarty/tech-blog",
-        gif: "./public/images/tech_blog.gif"
+        gif: "/public/images/tech_blog.gif"
     }
 ]
 
