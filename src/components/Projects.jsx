aed3435f45a816deffe1381095e4316270624ba0
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-11-02 20:05:34 -0700

    added an i

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 
 const Project = () => {
   return projects.map((project, i) => (
-    <div className="card m-3 border border-2 bg-dark">
+    <div className="card m-3 border border-2 bg-dark" key={i}>
       <img src={project.gif} className="card-img-top" alt={project.title} />
       <div className="card-body text-light">
         <h5 className="card-title">{project.title}</h5>
