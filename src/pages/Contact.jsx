093ae2dd15db3bccf9b74066a16d5dec8514807e
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-11-10 08:32:58 -0700

    removed contact form submission functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
                     type='text'
                     placeholder='Message'
                 />
-                <input className='mx-4' type='submit' value="Send" />
+                <input className='mx-4' type='submit'/>
             </form>
         </div>
     )
